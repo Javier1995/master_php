758f597592f85ac9d1ae629eb4f3e70109c4b184
Javier1995 <quiroga218@gmail.com>
2018-10-25 21:16:51 -0500

    Archivo de cambiado

--- 15-ejecicios/ejercicio4.php
+++ 15-ejecicios/ejercicio4.php
@@ -3,15 +3,47 @@
 
 
 /**
- * Ejercicio 3. Programa que compruebe que una variable esta vacia y si esta vacia rellenarla con texto en minuscula y mostrarlo en mayuscula y negretias;
+ * Ejercicio 4. Crear un script en php que tenga 4 variables, una de tipi array, otra de tipo string, otra int y otra booleana y que imprima un mensaje segun el tipi de variable que sea 
  * 
  * 
  */
 
- $texto = "";
+ $variable1 = array("Hola");
 
- if(empty($texto)){
-    $texto = "tiene un texto";
- } 
+ $variable2 = "hola";
 
- echo "<strong>".strtoupper($texto)."</strong>";
+ $variable3 = 2;
+
+ $variable4 = true;
+
+ /* echo 'La variable $variable1 es :'.gettype($variable1)."<br>";
+ 
+ echo 'La variable $variable2 es :'.gettype($variable2)."<br>";
+ 
+ echo 'La variable $variable3 es :'.gettype($variable3)."<br>";
+ 
+ echo 'La variable $variable4 es :'.gettype($variable4)."<br>"; */
+
+ for( $i = 1; $i <= 4; $i++ ){
+      $datoString = '$variable'.$i;
+      $datoValue  = ${"variable$i"};
+
+      switch($datoValue){
+            case is_numeric($datoValue):
+                echo "La variable".$datoString;
+                break;
+            case is_array($datoValue):
+                echo "La variable".$datoString;
+                break;
+            case is_bool($datoValue):
+                echo "La variable".$datoString;
+                break;
+            case is_numeric($datoValue):
+                echo "La variable".$datoString;
+                break;
+      }
+
+
+ }
+
+ var_dump($GLOBALS);
